e0ecf16c6f092991df41fc360dc5fad4aa8bd521
Malebone2 <malebonesenzeko@gmail.com>
2025-09-26 09:49:34 +0200

    Added date and time zone

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,1 +1,26 @@
+function updateTime() {
+  //Johannesburg
+  let joburgElement = document.querySelector("#joburg");
+  let joburgDateElement = joburgElement.querySelector(".date");
+  let joburgTimeElement = joburgElement.querySelector(".time");
+  let joburgTime = moment().tz("Africa/Johannesburg");
 
+  joburgDateElement.innerHTML = joburgTime.format("MMMM Do YYYY");
+  joburgTimeElement.innerHTML = ` ${joburgTime.format(
+    `h:mm:ss`
+  )} <small>${joburgTime.format("A")}</small>`;
+
+  //San Francisco
+  let sanFranciscoElement = document.querySelector("#san-francisco");
+  let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
+  let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
+  let sanFranciscoTime = moment().tz("America/Los_Angeles");
+
+  sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("MMMM Do YYYY");
+  sanFranciscoTimeElement.innerHTML = ` ${sanFranciscoTime.format(
+    `h:mm:ss`
+  )} <small>${sanFranciscoTime.format("A")}</small>`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
