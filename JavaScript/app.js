5d6ef35a6c3ce0a4d03b51c3095041e833e213b9
Malebone2 <malebonesenzeko@gmail.com>
2025-09-26 15:13:16 +0200

    Added local time

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -13,20 +13,24 @@ function updateTime() {
   }
 
   //San Francisco
-  let sanFranciscoElement = document.querySelector("#san-francisco");
-  if (sanFranciscoElement) {
-    let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
-    let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
-    let sanFranciscoTime = moment().tz("America/Los_Angeles");
-    sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("MMMM Do YYYY");
-    sanFranciscoTimeElement.innerHTML = ` ${sanFranciscoTime.format(
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = ` ${losAngelesTime.format(
       `h:mm:ss`
-    )} <small>${sanFranciscoTime.format("A")}</small>`;
+    )} <small>${losAngelesTime.format("A")}</small>`;
   }
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "Current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
